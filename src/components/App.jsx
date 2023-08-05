5caf4dc744d497b1690fe2937442a10427979b45
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-08-06 00:49:46 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,87 @@
-import { Feedback } from '../components/Feedback/Feedback';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Section } from 'components/Section/Section';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Notification } from 'components/Notification/Notification';
+import { Statistics } from 'components/Statistics/Statistics';
 
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-    </div>
-  );
-};
+export class App extends React.Component {
+  static defaultProps = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positivePercentage: 0,
+  };
+
+  static propTypes = {
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired,
+  };
+
+  state = {
+    good: this.props.good,
+    neutral: this.props.neutral,
+    bad: this.props.bad,
+  };
+
+  options = ['Good', 'Neutral', 'Bad'];
+
+  visible = false;
+
+  onIncrement = event => {
+    this.setState(prevState => {
+      this.visible = true;
+      const targetValue = event.target.id.toLowerCase();
+      return {
+        [targetValue]: Number.parseInt([prevState[targetValue]]) + 1,
+      };
+    });
+  };
+
+  onTotal = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  onPositivePercentage = () => {
+    let percent = 0;
+    if (this.state.good === 0) {
+      return percent;
+    } else {
+      percent = Math.round(
+        (this.state.good /
+          (this.state.good + this.state.neutral + this.state.bad)) *
+          100
+      );
+      return percent;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.options}
+            onLeaveFeedback={this.onIncrement}
+          />
+          {!this.visible ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              title="Statistics"
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.onTotal()}
+              positivePercentage={this.onPositivePercentage()}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
